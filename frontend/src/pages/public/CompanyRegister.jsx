553c8a4f4dd418a27ce71bd619da8e96a4ac4e7a
dineshkhichar569 <dineshkhichar569@gmail.com>
2025-12-31 17:26:05 +0530

    register frontend completed with admin registeration

--- frontend/src/pages/public/CompanyRegister.jsx
+++ frontend/src/pages/public/CompanyRegister.jsx
@@ -29,7 +29,7 @@ const SelectOption = ({ options, placeholder, onOptionSelection }) => {
   return (
     <div className="relative w-64">
       <button
-        className="cursor-pointer w-full flex justify-between items-center px-4 py-3 rounded-xl border bg-[#eceef4] shadow-sm text-gray-600 hover:shadow-md transition"
+        className="cursor-pointer w-full flex justify-between items-center px-4 py-2 rounded-xl border bg-[#eceef4] shadow-sm text-gray-600 hover:shadow-md transition"
         onClick={() => setOpen(!open)} ////// open will be true when button is clicked
       >
         {selected || placeholder}
@@ -65,24 +65,36 @@ function CompanyRegister() {
   const [companySize, setCompanySize] = useState("");
 
   return (
-    <main className="h-screen bg-[#eceef4]">
-      <div>
-        <h1>Get started with Jobrix</h1>
-        <p>Set up your company workspace and start hiring smarter</p>
+    <main className="h-full bg-[#eceef4] flex flex-col items-center">
+      <div className="space-y-2">
+        <h1 className="text-4xl font-bold">Get started with Jobrix</h1>
+        <p className="text-base text-gray-500">
+          Set up your company workspace and start hiring smarter
+        </p>
       </div>
 
-      <div className="bg-white m-10 p-8 rounded-2xl shadow-xl">
+      <div className="bg-white m-10 p-8 rounded-2xl shadow-xl  w-[50%]">
         <form className="flex flex-col gap-10" action="submit">
           <div className="flex flex-col gap-6">
-            <div>
-              <h1 className="font-semibold text-2xl">Company Details</h1>
-              <p className="text-gray-500 text-sm">Tell us about your organization</p>
+            <div className="flex justify-start items-center gap-3">
+              <img
+                className="border-2 border-blue-600 p-5 rounded-full"
+                src="#"
+                alt=""
+              />
+              <div>
+                <h1 className="font-semibold text-2xl">Company Details</h1>
+                <p className="text-gray-500 text-sm">
+                  Tell us about your organization
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col justify-center gap-6">
+
+            <div className="flex flex-col justify-center gap-6 pl-14">
               <label className="flex flex-col gap-3">
                 <p className="font-medium"> Company Name *</p>
                 <input
-                  className="bg-[#eceef4] p-3 rounded-xl border border-gray-400"
+                  className="bg-[#eceef4] p-2 rounded-lg border border-gray-400"
                   type="text"
                   placeholder="Joobrix Inc."
                   required
@@ -91,7 +103,7 @@ function CompanyRegister() {
               <label className="flex flex-col gap-3">
                 <p className="font-medium">Company Domain *</p>
                 <input
-                  className="bg-[#eceef4] p-3 rounded-xl border border-gray-400"
+                  className="bg-[#eceef4] p-2 rounded-lg border border-gray-400"
                   type="text"
                   placeholder="jobrix.com"
                   required
@@ -118,7 +130,7 @@ function CompanyRegister() {
               <label className="flex flex-col gap-3">
                 <p className="font-medium">Location</p>
                 <input
-                  className="bg-[#eceef4] p-3 rounded-xl border border-gray-400"
+                  className="bg-[#eceef4] p-2 rounded-lg border border-gray-400"
                   type="text"
                   placeholder="sikar, rajasthan"
                   required
@@ -130,44 +142,55 @@ function CompanyRegister() {
           <hr className="border-[1px]" />
 
           <div className="flex flex-col gap-6">
-            <div>
-              <h1 className="font-semibold text-2xl">Admin Account</h1>
-              <p className="text-gray-500 text-sm">Create your admministrator login</p>
+            <div className="flex justify-start items-center gap-3">
+              <img
+                className="border-2 border-blue-600 p-5 rounded-full"
+                src="#"
+                alt=""
+              />
+              <div>
+                <h1 className="font-semibold text-2xl">Admin Account</h1>
+                <p className="text-gray-500 text-sm">
+                  Create your admministrator login
+                </p>
+              </div>
             </div>
-            <label className="flex flex-col gap-3">
-              Full Name *
-              <input
-                className="bg-[#eceef4] p-3 rounded-xl border border-gray-400"
-                type="text"
-                placeholder="John Doe"
-                required
-              />
-            </label>
-            <label className="flex flex-col gap-3">
-              Work Email *
-              <input
-                className="bg-[#eceef4] p-3 rounded-xl border border-gray-400"
-                type="email"
-                placeholder="jhon@jobrix.com"
-                required
-              />
-            </label>
-            <label className="flex flex-col gap-3">
-              Password *
-              <input
-                className="bg-[#eceef4] p-3 rounded-xl border border-gray-400"
-                type="password"
-                placeholder="Min. 6 characters"
-                required
-              />
-            </label>
+            <div className="flex flex-col gap-6 pl-14">
+              <label className="flex flex-col gap-3">
+                Full Name *
+                <input
+                  className="bg-[#eceef4] p-2 rounded-lg border border-gray-400"
+                  type="text"
+                  placeholder="John Doe"
+                  required
+                />
+              </label>
+              <label className="flex flex-col gap-3">
+                Work Email *
+                <input
+                  className="bg-[#eceef4] p-2 rounded-lg border border-gray-400"
+                  type="email"
+                  placeholder="jhon@jobrix.com"
+                  required
+                />
+              </label>
+              <label className="flex flex-col gap-3">
+                Password *
+                <input
+                  className="bg-[#eceef4] p-2 rounded-lg border border-gray-400"
+                  type="password"
+                  placeholder="Min. 6 characters"
+                  required
+                />
+              </label>
+            </div>
           </div>
 
           <button>Create Company & Continue</button>
         </form>
-        <p>
-          By signing up, you agree to our <Link to="">Terms of Service</Link>{" "}
-          and <Link to="">Privacy Policy</Link>
+        <p className="text-center">
+          By signing up, you agree to our <Link to="" className="text-blue-600">Terms of Service</Link>{" "}
+          and <Link to="" className="text-blue-600">Privacy Policy</Link>
         </p>
       </div>
     </main>
