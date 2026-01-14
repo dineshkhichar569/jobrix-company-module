799c67437f452a5f0b722b52aee0e6495794a4f8
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-01-14 23:47:25 +0530

    all middlewares ar completed

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -1,4 +1,5 @@
 import authMiddleware from "./auth.middleware.js";
 import roleMiddleware from "./role.middleware.js";
+import companyMiddleware from "./company.middleware.js";
 
-export { authMiddleware, roleMiddleware };
+export { authMiddleware, roleMiddleware, companyMiddleware };
