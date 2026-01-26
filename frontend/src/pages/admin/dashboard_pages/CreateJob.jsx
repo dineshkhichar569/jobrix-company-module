657e317b73fecbb5b5fc0ad3868f6ba7a89aa6bd
dineshkhichar569 <dineshkhichar569@gmail.com>
2026-01-27 03:56:49 +0530

    Job page frontend completed

--- frontend/src/pages/admin/dashboard_pages/CreateJob.jsx
+++ frontend/src/pages/admin/dashboard_pages/CreateJob.jsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import { MapPin, Users2 } from "lucide-react";
+import React, { useState } from "react";
 
 function CreateJob() {
+  const [open, setOpen] = useState(false);
+  const [jobs, setJobs] = useState([]);
   return (
-    <div>
-      This is Job Creation Page....
+    <div className="space-y-8">
+      {/* Heading and add team job button */}
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-xl font-bold">Jobs</h1>
+          <p className="text-gray-600 text-sm">
+            Manage your jobs posting and track applicants.
+          </p>
+        </div>
+
+        {/* add team Member */}
+        <div>
+          <button
+            onClick={() => setOpen(!open)}
+            className="bg-indigo-600 text-white p-1 px-2 rounded-lg text-center flex gap-3"
+          >
+            <i className="font-medium">+</i> Create Job
+          </button>
+
+          {/* Add Member PopUp */}
+          <div>
+            {/* for background blur */}
+            <div
+              className={`fixed inset-0 z-50 bg-black/30 backdrop-blur-sm
+              transition-opacity duration-300
+              ${open ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}
+              `}
+              onClick={() => setOpen(false)}
+            />
+
+            {/* <AddMemberCard open={open} setOpen={setOpen} /> */}
+          </div>
+        </div>
+      </div>
+
+      {/* jobs */}
+      <div className="overflow-hidden rounded-xl border-[1px]">
+        <table className="w-full text-xs">
+          <thead className="bg-gray-100 text-gray-500 border-b">
+            <tr>
+              <th className="px-3 py-2 text-left">JOB TITLE</th>
+              <th className="px-3 py-2 text-left">DEPARTMENT</th>
+              <th className="px-3 py-2 text-left">LOCATION</th>
+              <th className="px-3 py-2 text-center">STATUS</th>
+              <th className="px-3 py-2 text-center">RECRUITER</th>
+              <th className="px-3 py-2 text-left">APPLICANTS</th>
+              <th className="px-3 py-2 text-left">CREATED</th>
+              <th className="px-3 py-2 text-left"></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {jobs.map((job) => (
+              <tr key={job._id} className="border-b hover:bg-slate-100">
+                <td className=" px-3 py-4 flex gap-3 items-center">
+                  <p className="font-medium text-base">{job.fullname}</p>
+                  <p className="text-[11px] text-blue-600">● {job.type}</p>
+                </td>
+                <td className="px-3 py-3 font-medium text-gray-500">
+                  {job.department}
+                </td>
+                <td className="px-3 py-3 font-medium text-gray-500 flex items-center gap-1">
+                  <MapPin className="w-4 h-4 text-indigo-500" /> {job.location}
+                </td>
+                <td className="px-3 py-3 text-center font-medium text-gray-500">
+                  <span
+                    className={`border rounded-lg px-2 p-[1px] text-[10px] font-medium
+                       ${job.status === "close" ? "text-red-600 bg-red-50 border-red-500" : "text-blue-600 bg-blue-50 border-blue-500"}`}
+                  >
+                    {job.status}
+                  </span>
+                </td>
+                <td className="px-3 py-3 text-center text-black font-medium">
+                  {job.recruiter}
+                </td>
+                <td className="px-3 py-3 text-center text-black font-medium flex items-center gap-3">
+                  <Users2 className="w-4 h-4 text-gray-500" /> 45
+                </td>
+                <td className="px-3 py-3 font-medium text-gray-500">
+                  Jan 15, 2023
+                </td>
+                <td className="px-3 py-3 cursor-pointer">•••</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
 export default CreateJob;
